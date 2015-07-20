f3ca4a0c2929954cfb07a7eafe67ee726a633fe0
Philipp Blume <pgbluster@gmail.com>
2015-07-20 00:07:37 -0500

    set indent to 2 spaces rather than 4

--- DurationVisualizer.rb
+++ DurationVisualizer.rb
@@ -1,26 +1,26 @@
 
 class Numeric
-	def round_to( decimals=0 )
-		factor = 10.0**decimals
-		(self*factor).round / factor
-	end
+  def round_to( decimals=0 )
+    factor = 10.0**decimals
+    (self*factor).round / factor
+  end
 end
 
 class Integer
-	def get_exploder_values
-		primevals = Array.new
-		1.upto(self / 2) { |x|
-			if self.relprime(x)
-				primevals.push(x)
-			end
-		}
-		return primevals
-	end
+  def get_exploder_values
+    primevals = Array.new
+    1.upto(self / 2) { |x|
+      if self.relprime(x)
+        primevals.push(x)
+      end
+    }
+    return primevals
+  end
 
-	def relprime(other)
-		t = Rational(self,other)
-		return t.numerator == self
-	end
+  def relprime(other)
+    t = Rational(self,other)
+    return t.numerator == self
+  end
 end
 
 class Array
@@ -30,40 +30,40 @@ class Array
   end
 
   def explode(n)
-  	this = []
-  	1.upto(self.length) do |x|
-  		k = self[(x* n-1 ) % self.length]
-  		this.push(k)
-  	end
-  	return this
+    this = []
+    1.upto(self.length) do |x|
+      k = self[(x* n-1 ) % self.length]
+      this.push(k)
+    end
+    return this
   end
 
   def collect_explosions
-  	exploded_hash = Hash.new
-  	selection = self.length.get_exploder_values
-  	selection.each {|x| exploded_hash.store(x, self.explode(x))}
-  	return exploded_hash
+    exploded_hash = Hash.new
+    selection = self.length.get_exploder_values
+    selection.each {|x| exploded_hash.store(x, self.explode(x))}
+    return exploded_hash
   end
 
 end
 
 def exploded_array(input_array, index, startval, endval, precision)
-	totval = 0.0
-	thing = []
-	this = []
-	runningtot = []
-	1.upto((input_array.length)) do |x|
-		k=input_array[(x*index-1) % input_array.length]
-		nextvalue = startval * (endval/startval)**((k-1)/(input_array.length-1.to_f))
-		nexttrunc = nextvalue.round_to(precision)
-		this.push(nexttrunc)
-		totval+=nextvalue
-		runningtot.push(totval.round_to(precision))
-	end
-	thing.push(this)
-	tottrunc = totval.round_to(precision)
-	thing.push(tottrunc)
-	thing.push(runningtot)
+  totval = 0.0
+  thing = []
+  this = []
+  runningtot = []
+  1.upto((input_array.length)) do |x|
+    k=input_array[(x*index-1) % input_array.length]
+    nextvalue = startval * (endval/startval)**((k-1)/(input_array.length-1.to_f))
+    nexttrunc = nextvalue.round_to(precision)
+    this.push(nexttrunc)
+    totval+=nextvalue
+    runningtot.push(totval.round_to(precision))
+  end
+  thing.push(this)
+  tottrunc = totval.round_to(precision)
+  thing.push(tottrunc)
+  thing.push(runningtot)
 end
 
 my_array = Array.new
@@ -73,12 +73,12 @@ ok_user = false
 1.upto(durelements) { |x| my_array.push(x) }
 
 while ok_user == false
-	my_array.shuffle!.display
-	puts "Is this ok?"
-	if /[yY]/.match(gets.chomp)
-		ok_user == true
-		break
-	end
+  my_array.shuffle!.display
+  puts "Is this ok?"
+  if /[yY]/.match(gets.chomp)
+    ok_user == true
+    break
+  end
 end
 
 hashout = my_array.collect_explosions
@@ -95,16 +95,16 @@ final = gets.chomp!.to_f
 puts "Provide the precision"
 places = gets.chomp!.to_f
 while ok_user == false
-	my_array = [1,2,3,4,5,6,8,9,10,11,12,13].shuffle
-	my_array.push(7)
-	my_array.display
-	puts "Is this ok?"
-	if /[yY]/.match(gets.chomp)
-		ok_user == true
-		break
-	end
+my_array = [1,2,3,4,5,6,8,9,10,11,12,13].shuffle
+my_array.push(7)
+my_array.display
+puts "Is this ok?"
+if /[yY]/.match(gets.chomp)
+ok_user == true
+break
+end
 end
 puts ""
 1.upto(12) {|x| puts exploded_array(my_array, x, initial, final, places).display
-	}
+}
 =end
